5b65662054fa933baeb522e2852b3fbeb1dae4e8
Anvarbek <anvarbekxaydarov777@gmail.com>
2023-09-15 14:45:33 +0500

    first commit2

--- src/app/api/auth/[...nextauth]/option.ts
+++ src/app/api/auth/[...nextauth]/option.ts
@@ -10,6 +10,7 @@ interface GoogleProviderTy {
 }
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { withAuth } from "next-auth/middleware"
 
 // More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
 export default withAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     authorized({ req, token }) {
       // `/admin` requires admin role
