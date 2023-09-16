2a4a44e289e9c514a4c40738353fa7d26bde8694
Anvarbek <anvarbekxaydarov777@gmail.com>
2023-09-16 11:56:54 +0500

    change-5

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-      NEXTAUTH_SECRET: 'Ey7nTKnggBc0bRN8WUjyShw2qzOZ6KW4fUyqcKBePxY=',
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
 };
 
