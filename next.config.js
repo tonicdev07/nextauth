399a9ebd29032f2b75285bb8b4aa46ffc1a5c6f8
Anvarbek <anvarbekxaydarov777@gmail.com>
2023-09-16 14:37:19 +0500

    finished complete

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ dotenv.config();
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: 'https://cdn.jsdelivr.net/gh/dheereshagrwal/colored-icons@1.6.8/ci.min.css',
   env: {
     NEXTAUTH_SECRET: "Ey7nTKnggBc0bRN8WUjyShw2qzOZ6KW4fUyqcKBePxY=",
   },
