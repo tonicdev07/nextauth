52c535943deede1e0e5cc731061b33bc437893fb
Anvarbek <anvarbekxaydarov777@gmail.com>
2023-09-16 12:25:56 +0500

    change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Header from "./components/header";
-import Footer from "./components/footer";
 import Provider from "./context/authProvider";
 
 const inter = Inter({ subsets: ["latin"] });
